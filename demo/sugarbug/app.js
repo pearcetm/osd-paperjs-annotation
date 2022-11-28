597d3124c25500562e0e103e20d06696a063d5d0
Tom <tmpearce@gmail.com>
2022-11-27 19:08:59 -0500

    moved demo

--- demo/sugarbug/app.js
+++ demo/sugarbug/app.js
@@ -1,5 +1,5 @@
 
-import { AnnotationToolkit } from '../js/annotationtoolkit.js';
+import { AnnotationToolkit } from '../../js/annotationtoolkit.js';
 let styledef;
 $.get('./init.geoJSON').then(x=>{
     styledef=x;
