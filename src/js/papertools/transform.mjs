b744895d16d387dbc949a0c02c82acb57a4bc87f
Tom <tmpearce@gmail.com>
2024-03-09 14:37:17 -0500

    bugfix with tolerance testing

--- src/js/papertools/transform.mjs
+++ src/js/papertools/transform.mjs
@@ -292,7 +292,7 @@ class TransformTool extends AnnotationUITool{
             fill:true,
             stroke:true,
             segments:true,
-            tolerance:this.getTolerance(5, item),
+            tolerance:this.getTolerance(5),
             match:i=>i.item.isGeoJSONFeature || i.item.parent.isGeoJSONFeature,
         })
         if(hitResult && !hitResult.item.isGeoJSONFeature){
