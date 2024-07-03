1973f77040caed00cd0de985c56ac6da64cbf597
Tom <tmpearce@gmail.com>
2024-07-03 12:16:15 -0400

    increment version to 0.4.8

--- src/js/annotationtoolkit.mjs
+++ src/js/annotationtoolkit.mjs
@@ -1,6 +1,6 @@
 /**
  * OpenSeadragon annotation plugin based on paper.js
- * @version 0.4.7
+ * @version 0.4.8
  * 
  * Includes additional open source libraries which are subject to copyright notices
  * as indicated accompanying those segments of code.
