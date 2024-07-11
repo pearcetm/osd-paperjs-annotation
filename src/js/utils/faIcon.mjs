007060ca5e4f4b144f0456c475fc2255942d80bc
Tom <tmpearce@gmail.com>
2024-07-11 13:27:05 -0400

    increment version to 0.4.10

--- src/js/utils/faIcon.mjs
+++ src/js/utils/faIcon.mjs
@@ -1,6 +1,6 @@
 /**
  * OpenSeadragon paperjs overlay plugin based on paper.js
- * @version 0.4.9
+ * @version 0.4.10
  * 
  * Includes additional open source libraries which are subject to copyright notices
  * as indicated accompanying those segments of code.
